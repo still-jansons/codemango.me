35cd73ec46f85982d56c2c04c5ca3712d0c7a484
Arturs Jansons <arturs.jansons@Arturs-M1-Pro.Home>
2022-04-29 19:07:57 +0100

    ADD LOGO

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial/>
+    <div class="logo-wrapper">
+        <img src="~/static/codemango.svg" alt="CODE MANGO">
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+    name: 'IndexPage'
 }
 </script>
+
+<style scoped>
+    .logo-wrapper {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .logo-wrapper img {
+        max-width: 90%;
+    }
+</style>
