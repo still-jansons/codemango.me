9cb0b641ab69b2716a90b9b773ac8a1a90187bbf
Arturs Jansons <arturs.jansons@Arturs-M1-Pro.Home>
2022-05-02 18:49:35 +0100

    Background update

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-    <div class="w-screen h-screen flex items-center justify-center">
-        <img class="max-w-[90%]" src="~/static/codemango.svg" alt="CODE MANGO">
+    <div class="overflow-hidden" :style="bg_fluid">
+        <div class="w-screen h-screen flex items-center justify-center" :style="bg_glass">
+            <img class="max-w-[90%]" src="/codemango.svg" alt="CODE MANGO">
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'IndexPage'
+    name: 'IndexPage',
+    data() {
+        return {
+            bg_fluid: {
+                backgroundColor: 'white',
+                backgroundImage: "url(/fluid.svg)"
+            },
+            bg_glass: {
+                background: 'rgba( 255, 255, 255, 0 )',
+                backdropFilter: 'blur( 8px )',
+                '-webkit-backdrop-filter': 'blur( 8px )',
+                filter: 'saturate(120%)'
+            }
+        }
+    }
 }
 </script>
